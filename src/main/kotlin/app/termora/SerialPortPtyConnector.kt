5934b64f07d3e8a3f5b3a9490212112919a1081e
hstyi <spatter.flushes1o@icloud.com>
2025-01-27 16:53:30 +0800

    feat: serial charset

--- src/main/kotlin/app/termora/SerialPortPtyConnector.kt
+++ src/main/kotlin/app/termora/SerialPortPtyConnector.kt
@@ -10,17 +10,17 @@ class SerialPortPtyConnector(
     private val serialPort: SerialPort,
     private val charset: Charset = Charsets.UTF_8
 ) : PtyConnector {
+
+    private val buffer = ByteArray(1)
     private val reader = InputStreamReader(object : InputStream() {
         override fun read(): Int {
-            val buffer = ByteArray(1)
             return when (val len = serialPort.readBytes(buffer, buffer.size)) {
                 0 -> 0
-                1 -> buffer[0].toInt()
+                1 -> buffer.first().toInt()
                 else -> len
             }
         }
-
-    })
+    }, charset)
 
     override fun read(buffer: CharArray): Int {
         return reader.read(buffer, 0, buffer.size)
@@ -28,6 +28,10 @@ class SerialPortPtyConnector(
 
     override fun write(buffer: ByteArray, offset: Int, len: Int) {
         serialPort.writeBytes(buffer, offset, len)
+    }
+
+    override fun write(buffer: String) {
+        write(charset.encode(buffer).array())
     }
 
     override fun resize(rows: Int, cols: Int) {
--- src/main/kotlin/app/termora/SerialTerminalTab.kt
+++ src/main/kotlin/app/termora/SerialTerminalTab.kt
@@ -1,10 +1,15 @@
 package app.termora
 
 import app.termora.terminal.PtyConnector
+import org.apache.commons.io.Charsets
+import java.nio.charset.StandardCharsets
 
 class SerialTerminalTab(windowScope: WindowScope, host: Host) : PtyHostTerminalTab(windowScope, host) {
     override suspend fun openPtyConnector(): PtyConnector {
         val serialPort = Serials.openPort(host)
-        return SerialPortPtyConnector(serialPort)
+        return SerialPortPtyConnector(
+            serialPort,
+            Charsets.toCharset(host.options.serialComm.charset, StandardCharsets.UTF_8)
+        )
     }
 }
