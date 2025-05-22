8fb99c11ebd73cc6ab784c457c59fdf52de6ff9b
hstyi <hstyi@foxmail.com>
2025-05-22 17:12:31 +0800

    添加备用字段

--- src/main/kotlin/app/termora/db/Data.kt
+++ src/main/kotlin/app/termora/db/Data.kt
@@ -2,6 +2,7 @@ package app.termora.db
 
 import app.termora.LocalSecret
 import app.termora.randomUUID
+import org.apache.commons.lang3.StringUtils
 import org.jetbrains.exposed.v1.core.Column
 import org.jetbrains.exposed.v1.core.Table
 import org.jetbrains.exposed.v1.crypt.Algorithms
@@ -12,7 +13,7 @@ object Data : Table() {
     /**
      * 归属者
      */
-    val ownerId: Column<String> = char("ownerId", length = 32)
+    val ownerId: Column<String> = char("ownerId", length = 32).index()
 
     /**
      * [OwnerType]
@@ -33,6 +34,15 @@ object Data : Table() {
      * 是否已经同步标识，每次更新都要设置成 false 否则不会同步
      */
     val synced: Column<Boolean> = bool("synced").clientDefault { false }
+
+    /**
+     * 备用字段1-5
+     */
+    val extra1: Column<String> = text("extra1").clientDefault { StringUtils.EMPTY }
+    val extra2: Column<String> = text("extra2").clientDefault { StringUtils.EMPTY }
+    val extra3: Column<String> = text("extra3").clientDefault { StringUtils.EMPTY }
+    val extra4: Column<String> = text("extra4").clientDefault { StringUtils.EMPTY }
+    val extra5: Column<String> = text("extra5").clientDefault { StringUtils.EMPTY }
 
     /**
      * 数据
