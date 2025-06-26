17082c5fb8087575d602ef95d45dddf049083a1c
hstyi <hstyi@foxmail.com>
2025-06-26 09:34:11 +0800

    chore: transfer supports custom type

--- plugins/s3/src/main/kotlin/app/termora/plugins/s3/S3Plugin.kt
+++ plugins/s3/src/main/kotlin/app/termora/plugins/s3/S3Plugin.kt
@@ -1,8 +1,5 @@
 package app.termora.plugins.s3
 
-import app.termora.DynamicIcon
-import app.termora.I18n
-import app.termora.Icons
 import app.termora.plugin.Extension
 import app.termora.plugin.ExtensionSupport
 import app.termora.plugin.PaidPlugin
