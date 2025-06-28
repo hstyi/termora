a8201f8d894b4064ca178fecb681f398fc1b4329
hstyi <hstyi@foxmail.com>
2025-06-28 11:07:39 +0800

    fix: compile error

--- plugins/geo/build.gradle.kts
+++ plugins/geo/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.org.apache.commons.io.FileUtils
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
@@ -12,18 +10,6 @@ dependencies {
     implementation("com.maxmind.geoip2:geoip2:4.3.1")
     // https://github.com/hstyi/geolite2
     implementation("com.github.hstyi:geolite2:v1.0-202506280303")
-}
-
-/**
- * Download GeoLite2-Country.mmdb
- */
-tasks.withType<Jar> {
-    val file = file("${project.layout.buildDirectory.get().asFile.absolutePath}/GeoLite2-Country.mmdb")
-    if (file.exists().not()) {
-        FileUtils.forceMkdirParent(file)
-        ant.invokeMethod("get", mapOf("src" to "https://git.io/GeoLite2-Country.mmdb", "dest" to file.absolutePath))
-    }
-    from(file.absolutePath) { into(".") }
 }
 
 apply(from = "$rootDir/plugins/common.gradle.kts")
