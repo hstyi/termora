d1dba56bcd703534471cb863c11529f863b3c7c1
hstyi <hstyi@foxmail.com>
2025-07-04 16:27:11 +0800

    chore: improve sidebar

--- src/main/kotlin/app/termora/TermoraFencePanel.kt
+++ src/main/kotlin/app/termora/TermoraFencePanel.kt
@@ -1,13 +1,20 @@
 package app.termora
 
+import app.termora.actions.AnAction
+import app.termora.actions.AnActionEvent
 import app.termora.tree.NewHostTree
 import com.formdev.flatlaf.extras.components.FlatTabbedPane
+import com.formdev.flatlaf.extras.components.FlatToolBar
 import com.formdev.flatlaf.util.SystemInfo
 import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.Font
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
+import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import javax.swing.*
+import kotlin.math.max
 
 
 class TermoraFencePanel(
@@ -24,6 +31,8 @@ class TermoraFencePanel(
     private val leftTreePanel = LeftTreePanel()
     private val mySplitPane = JSplitPaneWithZeroSizeDivider(splitPane) { tabbed.tabHeight }
     private val enableManager get() = EnableManager.getInstance()
+    private val toolbar = FlatToolBar().apply { isFloatable = false }
+    private var dividerLocation = 0
 
     init {
         initView()
@@ -44,12 +53,39 @@ class TermoraFencePanel(
         tabbed.tabType = FlatTabbedPane.TabType.underlined
         tabbed.tabAreaInsets = null
 
+        // macOS 避开控制栏
+        if (SystemInfo.isMacOS) {
+            toolbar.add(Box.createHorizontalStrut(76))
+        }
+
+        toolbar.add(createColspanAction())
+        tabbed.leadingComponent = toolbar
+        toolbar.isVisible = false
+
         add(mySplitPane, BorderLayout.CENTER)
     }
 
     private fun initEvents() {
         Disposer.register(this, leftTreePanel)
         splitPane.addPropertyChangeListener("dividerLocation") { mySplitPane.doLayout() }
+
+        leftTreePanel.addComponentListener(object : ComponentAdapter() {
+            override fun componentHidden(e: ComponentEvent) {
+                toolbar.isVisible = true
+            }
+
+            override fun componentShown(e: ComponentEvent) {
+                toolbar.isVisible = false
+            }
+        })
+
+        actionMap.put("toggle", createColspanAction())
+        getInputMap(WHEN_IN_FOCUSED_WINDOW).put(
+            KeyStroke.getKeyStroke(
+                KeyEvent.VK_B,
+                toolkit.menuShortcutKeyMaskEx or KeyEvent.SHIFT_DOWN_MASK
+            ), "toggle"
+        )
     }
 
     private inner class LeftTreePanel : JPanel(BorderLayout()), Disposable {
@@ -70,9 +106,14 @@ class TermoraFencePanel(
             val label = JLabel(Application.getName())
             label.foreground = UIManager.getColor("textInactiveText")
             label.font = label.font.deriveFont(Font.BOLD)
+            // 与最后一个按钮对冲，使其宽度和谐
+            box.add(JButton(Icons.empty))
             box.add(Box.createHorizontalGlue())
-            if (SystemInfo.isMacOS.not()) box.add(label)
+            if (SystemInfo.isMacOS.not()) {
+                box.add(label)
+            }
             box.add(Box.createHorizontalGlue())
+            box.add(createColspanAction())
 
             if (SystemInfo.isMacOS || SystemInfo.isLinux) {
                 box.addMouseListener(moveMouseAdapter)
@@ -95,8 +136,24 @@ class TermoraFencePanel(
         }
     }
 
+    private fun createColspanAction(): Action {
+        return object : AnAction(Icons.dataColumn) {
+            init {
+                val text = I18n.getString("termora.welcome.toggle-sidebar")
+                putValue(SHORT_DESCRIPTION, "$text (${if (SystemInfo.isMacOS) '⌘' else "Ctrl"} + Shift + B)")
+            }
+
+            override fun actionPerformed(evt: AnActionEvent) {
+                if (leftTreePanel.isVisible) dividerLocation = splitPane.dividerLocation
+                leftTreePanel.isVisible = leftTreePanel.isVisible.not()
+                if (leftTreePanel.isVisible) splitPane.dividerLocation = dividerLocation
+            }
+        }
+    }
+
+
     override fun dispose() {
-        enableManager.setFlag("Termora.Fence.dividerLocation", splitPane.dividerLocation)
+        enableManager.setFlag("Termora.Fence.dividerLocation", max(splitPane.dividerLocation, 10))
     }
 
     fun getHostTree(): NewHostTree {
