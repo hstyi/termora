5347b0c01376f63744af9d562b126407b6e2db40
hstyi <hstyi@foxmail.com>
2025-07-20 12:15:53 +0800

    feat: support VNC protocol

--- src/main/kotlin/app/termora/DynamicIcon.kt
+++ src/main/kotlin/app/termora/DynamicIcon.kt
@@ -2,10 +2,22 @@ package app.termora
 
 import com.formdev.flatlaf.extras.FlatSVGIcon
 
-open class DynamicIcon(name: String, private val darkName: String = name, val allowColorFilter: Boolean = true) :
-    FlatSVGIcon(name) {
+open class DynamicIcon(
+    name: String,
+    private val darkName: String = name,
+    val allowColorFilter: Boolean = true,
+    loader: ClassLoader?
+) : FlatSVGIcon(name, loader) {
     constructor(name: String) : this(name, name)
 
-    val dark by lazy { DynamicIcon(darkName, name, allowColorFilter) }
+    constructor(
+        name: String,
+        darkName: String = name,
+        allowColorFilter: Boolean = true,
+    ) : this(name, darkName, allowColorFilter, null) {
+
+    }
+
+    val dark by lazy { DynamicIcon(darkName, name, allowColorFilter, classLoader) }
 
 }
