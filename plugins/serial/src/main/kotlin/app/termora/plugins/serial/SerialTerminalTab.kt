e21edd219ecdd37e6511cd3b04656c18212748bc
hstyi <hstyi@foxmail.com>
2025-08-01 17:47:12 +0800

    chore: serial terminal

--- plugins/serial/src/main/kotlin/app/termora/plugins/serial/SerialTerminalTab.kt
+++ plugins/serial/src/main/kotlin/app/termora/plugins/serial/SerialTerminalTab.kt
@@ -1,9 +1,6 @@
 package app.termora.plugins.serial
 
-import app.termora.Host
-import app.termora.Icons
-import app.termora.PtyHostTerminalTab
-import app.termora.WindowScope
+import app.termora.*
 import app.termora.terminal.PtyConnector
 import org.apache.commons.io.Charsets
 import java.nio.charset.StandardCharsets
@@ -11,12 +8,18 @@ import javax.swing.Icon
 
 class SerialTerminalTab(windowScope: WindowScope, host: Host) :
     PtyHostTerminalTab(windowScope, host) {
+
+
     override suspend fun openPtyConnector(): PtyConnector {
         val serialPort = Serials.openPort(host)
         return SerialPortPtyConnector(
             serialPort,
             Charsets.toCharset(host.options.encoding, StandardCharsets.UTF_8)
         )
+    }
+
+    override fun createReconnectTerminalTab(): TerminalTab {
+        return SerialTerminalTab(windowScope, host)
     }
 
     override fun getIcon(): Icon {
